86a1872fe1e27c37bb446631b29ff7e6438d232f
Parsa <parsaeini1998@gmail.com>
2026-02-08 15:00:38 +0330

    chore(main): fix cors error

--- app/main.py
+++ app/main.py
@@ -46,7 +46,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
